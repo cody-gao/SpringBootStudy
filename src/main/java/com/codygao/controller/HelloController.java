8ff743f6de09079f6fe5e51782ea69b779e23e57
gaoerpeng <gaoerpeng@didichuxing.com>
2019-11-13 14:38:48 +0800

    base function

---
+++ src/main/java/com/codygao/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.codygao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("userName","codygao");
+        model.addAttribute("phone","13269661133");
+        return "user";
+    }
+}
